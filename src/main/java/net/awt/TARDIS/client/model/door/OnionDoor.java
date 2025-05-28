09d872927b8c6b6ede091c176ae294ede98372fe
Lt-DO7 <alfieharrylansdell@gmail.com>
2025-05-28 23:12:03 +0100

    Onion

--- src/main/java/net/awt/TARDIS/client/model/door/OnionDoor.java
+++ src/main/java/net/awt/TARDIS/client/model/door/OnionDoor.java
@@ -17,23 +17,21 @@ import net.minecraft.util.math.RotationAxis;
 // Paste this class into your mod and generate all required imports
 public class OnionDoor extends DoorModel {
 	private final ModelPart Root;
-	private final ModelPart Body;
 	private final ModelPart OmgitsaUFO;
 	private final ModelPart GetInLoserWereGoingShopping;
 	public OnionDoor(ModelPart root) {
 		this.Root = root.getChild("Root");
-		this.Body = this.Root.getChild("Body");
-		this.OmgitsaUFO = this.Body.getChild("OmgitsaUFO");
+		this.OmgitsaUFO = this.Root.getChild("OmgitsaUFO");
 		this.GetInLoserWereGoingShopping = this.OmgitsaUFO.getChild("GetInLoserWereGoingShopping");
 	}
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData Root = modelPartData.addChild("Root", ModelPartBuilder.create(), ModelTransform.of(0.0F, -47.5F, 0.0F, 0.0F, 3.1416F, 0.0F));
+		ModelPartData Root = modelPartData.addChild("Root", ModelPartBuilder.create(), ModelTransform.of(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
-		ModelPartData Body = Root.addChild("Body", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 12.5F, 0.0F));
+		ModelPartData OmgitsaUFO = Root.addChild("OmgitsaUFO", ModelPartBuilder.create().uv(88, 44).cuboid(-5.0F, 0.0F, -5.0F, 10.0F, 35.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -35.0F, 0.0F));
 
-		ModelPartData OmgitsaUFO = Body.addChild("OmgitsaUFO", ModelPartBuilder.create().uv(88, 44).cuboid(-5.0F, 0.0F, -5.0F, 10.0F, 35.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+		ModelPartData Door_r1 = OmgitsaUFO.addChild("Door_r1", ModelPartBuilder.create().uv(96, 0).cuboid(-1.0F, -7.0F, -7.0F, 2.0F, 14.0F, 14.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 35.0F, 0.0F, -1.5708F, 0.0F, -1.5708F));
 
 		ModelPartData GetInLoserWereGoingShopping = OmgitsaUFO.addChild("GetInLoserWereGoingShopping", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 3.0F, 0.0F));
 
@@ -47,6 +45,15 @@ public class OnionDoor extends DoorModel {
 	@Override
 	public Animation getAnimationForDoorState(DoorHandler.AnimationDoorState animationDoorState) {
 		return null;
+	}
+
+	@Override
+	public void renderWithAnimations(ClientTardis tardis, AbstractLinkableBlockEntity doorEntity, ModelPart root, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float pAlpha) {
+		matrices.push();
+		matrices.scale(1F, 1F, 1F);
+		matrices.translate(0.0F, -1.5F, 0.0F);
+		super.renderWithAnimations(tardis, doorEntity, root, matrices, vertices, light, overlay, red, green, blue, pAlpha);
+		matrices.pop();
 	}
 
 	@Override
