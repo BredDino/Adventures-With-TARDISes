3b13791cd17a1759e1f4ec406b5a1572ccf63b69
BredDino <theminecraftdinoman@gmail.com>
2025-05-28 23:33:19 +0200

    tardis registry but cleaner

--- src/main/java/net/awt/AdventureWithTARDISesClient.java
+++ src/main/java/net/awt/AdventureWithTARDISesClient.java
@@ -1,10 +1,11 @@
 package net.awt;
 
+import net.awt.TARDIS.TardisRegistry;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AdventureWithTARDISesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        TardisRegistry.registerClientAddonExteriors();
     }
 }
