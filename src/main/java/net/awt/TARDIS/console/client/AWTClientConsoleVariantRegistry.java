b7fd97a94ba6ef331565c54d0ac2ce9ad4207554
BredDino <theminecraftdinoman@gmail.com>
2025-06-03 16:44:28 +0200

    this is a big one

--- src/main/java/net/awt/TARDIS/console/client/AWTClientConsoleVariantRegistry.java
+++ src/main/java/net/awt/TARDIS/console/client/AWTClientConsoleVariantRegistry.java
@@ -5,6 +5,7 @@ import dev.amble.ait.data.schema.console.ClientConsoleVariantSchema;
 public class AWTClientConsoleVariantRegistry  {
     public static ClientConsoleVariantSchema TEST;
     public static ClientConsoleVariantSchema DICE;
+    public static ClientConsoleVariantSchema JUNKGLASS;
 
     public static void init() {
     }
---
+++ src/main/java/net/awt/TARDIS/console/variants/JunkGlassVariant.java
@@ -0,0 +1,17 @@
+package net.awt.TARDIS.console.variants;
+
+
+import dev.amble.ait.data.schema.console.ConsoleVariantSchema;
+import net.awt.AdventuresWithTARDISes;
+import net.awt.TARDIS.console.types.DiceType;
+import net.awt.TARDIS.console.types.JunkGlassType;
+import net.minecraft.util.Identifier;
+
+public class JunkGlassVariant extends ConsoleVariantSchema {
+    public static final Identifier REFERENCE = AdventuresWithTARDISes.id("console/junkglass");
+
+    public JunkGlassVariant() {
+        super(JunkGlassType.REFERENCE, REFERENCE);
+    }
+
+}
