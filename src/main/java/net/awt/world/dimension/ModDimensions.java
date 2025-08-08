c8bfa7f7fd7b0edc904e32023c70e2be85ed3743
Deo <alfieharrylansdell@gmail.com>
2025-08-08 13:21:35 +0100

    WIP SKARODIM

---
+++ src/main/java/net/awt/world/dimension/ModDimensions.java
@@ -0,0 +1,42 @@
+package net.awt.world.dimension;
+import net.awt.AdventuresWithTARDISes;
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
+
+import java.util.OptionalLong;
+
+public class ModDimensions {
+    public static final RegistryKey<DimensionOptions> SKARO_KEY = RegistryKey.of(RegistryKeys.DIMENSION,
+            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
+    public static final RegistryKey<World> SKARO_LEVEL_KEY = RegistryKey.of(RegistryKeys.WORLD,
+            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
+    public static final RegistryKey<DimensionType> SKARO_DIM_TYPE = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
+            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro_type"));
+
+    public static void bootstrapType(Registerable<DimensionType> context) {
+        context.register(SKARO_DIM_TYPE, new DimensionType(
+                OptionalLong.of(12000), // fixedTime
+                false, // hasSkylight
+                false, // hasCeiling
+                false, // ultraWarm
+                true, // natural
+                1.0, // coordinateScale
+                true, // bedWorks
+                false, // respawnAnchorWorks
+                0, // minY
+                256, // height
+                256, // logicalHeight
+                BlockTags.INFINIBURN_OVERWORLD, // infiniburn
+                DimensionTypes.OVERWORLD_ID, // effectsLocation
+                1.0f, // ambientLight
+                new DimensionType.MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)));
+    }
+}
