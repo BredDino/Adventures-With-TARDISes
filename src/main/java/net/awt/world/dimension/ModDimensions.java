3ce7df235ba470a66bc8380a10b11f91771c30f0
Deo <alfieharrylansdell@gmail.com>
2025-08-10 20:41:37 +0100

    removed wip skaro

--- src/main/java/net/awt/world/dimension/ModDimensions.java
+++ src/main/java/net/awt/world/dimension/ModDimensions.java
@@ -14,12 +14,12 @@ import net.minecraft.world.dimension.DimensionTypes;
 import java.util.OptionalLong;
 
 public class ModDimensions {
-    public static final RegistryKey<DimensionOptions> SKARO_KEY = RegistryKey.of(RegistryKeys.DIMENSION,
-            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
-    public static final RegistryKey<World> SKARO_LEVEL_KEY = RegistryKey.of(RegistryKeys.WORLD,
-            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
-    public static final RegistryKey<DimensionType> SKARO_DIM_TYPE = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
-            new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro_type"));
+   // public static final RegistryKey<DimensionOptions> SKARO_KEY = RegistryKey.of(RegistryKeys.DIMENSION,
+   //         new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
+   // public static final RegistryKey<World> SKARO_LEVEL_KEY = RegistryKey.of(RegistryKeys.WORLD,
+    //        new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro"));
+   // public static final RegistryKey<DimensionType> SKARO_DIM_TYPE = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
+   //         new Identifier(AdventuresWithTARDISes.MOD_ID, "skaro_type"));
 
   //  public static final RegistryKey<DimensionOptions> ABANDONEDOVERWORLD_KEY = RegistryKey.of(RegistryKeys.DIMENSION,
    //         new Identifier(AdventuresWithTARDISes.MOD_ID, "abandonedoverworld"));
@@ -29,23 +29,23 @@ public class ModDimensions {
     //        new Identifier(AdventuresWithTARDISes.MOD_ID, "abandonedoverworld_type"));
 
 
-    public static void bootstrapType(Registerable<DimensionType> context) {
-        context.register(SKARO_DIM_TYPE, new DimensionType(
-                OptionalLong.of(12000), // fixedTime
-                false, // hasSkylight
-                false, // hasCeiling
-                false, // ultraWarm
-                true, // natural
-                1.0, // coordinateScale
-                true, // bedWorks
-                false, // respawnAnchorWorks
-                0, // minY
-                256, // height
-                256, // logicalHeight
-                BlockTags.INFINIBURN_OVERWORLD, // infiniburn
-                DimensionTypes.OVERWORLD_ID, // effectsLocation
-                1.0f, // ambientLight
-                new DimensionType.MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)));
+   // public static void bootstrapType(Registerable<DimensionType> context) {
+      //  context.register(SKARO_DIM_TYPE, new DimensionType(
+        //        OptionalLong.of(12000), // fixedTime
+         //       false, // hasSkylight
+          //      false, // hasCeiling
+           //     false, // ultraWarm
+           //     true, // natural
+           //     1.0, // coordinateScale
+            //    true, // bedWorks
+            //    false, // respawnAnchorWorks
+             //   0, // minY
+             //   256, // height
+             //   256, // logicalHeight
+              //  BlockTags.INFINIBURN_OVERWORLD, // infiniburn
+             //   DimensionTypes.OVERWORLD_ID, // effectsLocation
+              //  1.0f, // ambientLight
+               // new DimensionType.MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)));
 
        // context.register(ABANDONEDOVERWORLD_DIM_TYPE, new DimensionType(
        //         OptionalLong.of(12000), // fixedTime
@@ -63,5 +63,5 @@ public class ModDimensions {
            //     DimensionTypes.OVERWORLD_ID, // effectsLocation
            //     1.0f, // ambientLight
            //     new DimensionType.MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)));
-    }
+  //  }
 }
