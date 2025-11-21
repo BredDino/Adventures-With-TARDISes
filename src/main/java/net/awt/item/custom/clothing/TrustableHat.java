b82b815969f305fb9fc66582a8feb2045547c426
Deo <alfieharrylansdell@gmail.com>
2025-11-21 00:29:16 +0000

    trustablehat somewhat compat w armour stand

--- src/main/java/net/awt/item/custom/clothing/TrustableHat.java
+++ src/main/java/net/awt/item/custom/clothing/TrustableHat.java
@@ -3,6 +3,7 @@ package net.awt.item.custom.clothing;
 import dev.emi.trinkets.TrinketSlot;
 import dev.emi.trinkets.api.*;
 import dev.emi.trinkets.api.client.TrinketRenderer;
+import net.awt.item.ModItems;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
@@ -17,20 +18,17 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
-import net.minecraft.item.Equipment;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
-import net.minecraft.util.ClickType;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.*;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
@@ -40,23 +38,88 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public class TrustableHat extends Item implements Trinket, TrinketRenderer {
+public class TrustableHat extends ArmorItem implements Trinket, TrinketRenderer {
+
     public static final String HAT_HEIGHT_KEY = "hat_height_key";
 
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity player, LivingEntity entity, Hand hand) {
+        if (entity instanceof ArmorStandEntity) {
+            ArmorStandEntity armorStand = (ArmorStandEntity) entity;
+
+            armorStand.equipStack(EquipmentSlot.HEAD, new ItemStack(ModItems.TRUSTABLE_HAT));
+
+            if (!player.getWorld().isClient) {
+                stack.decrement(1);
+            }
+
+            return ActionResult.SUCCESS;
+        }
+
+        return ActionResult.PASS;
+    }
+
+
+
     public TrustableHat(Settings settings) {
-        super(settings);
+        super(ArmorMaterials.LEATHER, ArmorItem.Type.HELMET, settings);
         TrinketsApi.registerTrinket(this, this);
     }
 
+
     @Override
-    public void render(ItemStack stack, SlotReference slotReference, EntityModel<? extends LivingEntity> contextModel, MatrixStack matrixStack, VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+    public void render(
+            ItemStack stack,
+            SlotReference slotReference,
+            EntityModel<? extends LivingEntity> contextModel,
+            MatrixStack matrixStack,
+            VertexConsumerProvider vertexConsumers,
+            int light,
+            LivingEntity entity,
+            float limbAngle,
+            float limbDistance,
+            float tickDelta,
+            float animationProgress,
+            float headYaw,
+            float headPitch
+    ) {
         ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-        TrinketRenderer.translateToFace(matrixStack, (PlayerEntityModel<AbstractClientPlayerEntity>) contextModel, (AbstractClientPlayerEntity) entity, headYaw, headPitch);
-        matrixStack.translate(0,0.15,0.3);
+
+
+        if (entity instanceof AbstractClientPlayerEntity clientPlayer &&
+                contextModel instanceof PlayerEntityModel<?> playerModel) {
+
+            TrinketRenderer.translateToFace(
+                    matrixStack,
+                    (PlayerEntityModel<AbstractClientPlayerEntity>) playerModel,
+                    clientPlayer,
+                    headYaw,
+                    headPitch
+            );
+        } else {
+            // ✔️ Armor stand / other entities: manually position the item
+            matrixStack.translate(0, -0.2, 0); // adjust if needed
+        }
+
+        // Common transforms
+        matrixStack.translate(0, 0.15, 0.3);
         matrixStack.multiply(RotationAxis.NEGATIVE_Z.rotationDegrees(180.0F));
-        itemRenderer.renderItem(entity, stack, ModelTransformationMode.HEAD, false, matrixStack, vertexConsumers, entity.getWorld(), light, OverlayTexture.DEFAULT_UV, 0);
-        matrixStack.scale(0,0,0);
+
+        itemRenderer.renderItem(
+                entity,
+                stack,
+                ModelTransformationMode.HEAD,
+                false,
+                matrixStack,
+                vertexConsumers,
+                entity.getWorld(),
+                light,
+                OverlayTexture.DEFAULT_UV,
+                0
+        );
     }
+
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
@@ -109,6 +172,7 @@ public class TrustableHat extends Item implements Trinket, TrinketRenderer {
         }
         return false;
     }
+
     public static boolean hasEquipped(PlayerEntity player, Item item) {
         Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent(player);
 
