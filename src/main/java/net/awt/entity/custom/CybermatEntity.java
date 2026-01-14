09dbaeb90b75497d81da86035fee63a523bec010
Deo <alfieharrylansdell@gmail.com>
2026-01-14 01:21:39 +0000

    AWT:Fabricated Made Cybermat able to spawn and work. Just does not spawn naturally yet

--- src/main/java/net/awt/entity/custom/CybermatEntity.java
+++ src/main/java/net/awt/entity/custom/CybermatEntity.java
@@ -1,25 +1,24 @@
 package net.awt.entity.custom;
 
-import net.awt.entity.ModEntities;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.mob.SilverfishEntity;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 
-public class CybermatEntity extends HostileEntity {
+public class CybermatEntity extends SilverfishEntity {
 
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
-    public CybermatEntity(EntityType<? extends HostileEntity> entityType, World world) {
-        super((EntityType<? extends HostileEntity>) entityType, world);
+    public CybermatEntity(EntityType<? extends SilverfishEntity> entityType, World world) {
+        super(entityType, world);
     }
-
 
     private void setupAnimationStates() {
         if (this.idleAnimationTimeout <= 0) {
@@ -31,11 +30,6 @@ public class CybermatEntity extends HostileEntity {
     }
 
     @Override
-    protected void initDataTracker() {
-
-    }
-
-    @Override
     public void tick() {
         super.tick();
         if (this.getWorld().isClient) {
@@ -43,13 +37,31 @@ public class CybermatEntity extends HostileEntity {
         }
     }
 
-
     public static DefaultAttributeContainer.Builder createCybermatAttributes() {
-        return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 50)
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 10)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25f)
-                .add(EntityAttributes.GENERIC_ARMOR, 1);
+        return SilverfishEntity.createSilverfishAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 12.0)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4);
+    }
 
+    @Override
+    protected void initGoals() {
+        // Do NOT call super.initGoals(), it adds Silverfish-specific burrowing goal
+
+        // Standard mob AI
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 1.0, true));
+        this.goalSelector.add(2, new WanderAroundGoal(this, 1.0));
+        this.goalSelector.add(3, new LookAtEntityGoal(this, net.minecraft.entity.player.PlayerEntity.class, 6.0F));
+        this.goalSelector.add(4, new LookAroundGoal(this));
+
+        this.targetSelector.add(0, new RevengeGoal(this));
+        this.targetSelector.add(1, new net.minecraft.entity.ai.goal.ActiveTargetGoal<>(this, net.minecraft.entity.player.PlayerEntity.class, true));
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return null; // No hissing sound when idle
     }
 }
+
